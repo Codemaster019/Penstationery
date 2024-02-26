f387d38449aa02d714fca4f6ea2c369368bebf4e
Himanshu <himanshudey19@gmail.com>
2024-02-26 14:10:41 +0530

    updated initial commit black themed

--- src/Dashboard/dashboard.jsx
+++ src/Dashboard/dashboard.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import Lottie from 'lottie-react';
+import animationData from './dashani.json';
+import smoke from './smoke.mp4'
 import './dashboard.css';
 
 function Dashboard (){
     return (
+    
     <div>
-<div className='dashboard_div'>
-    <h1 className='header_text'>Leading the way to creativity</h1>
-    <p>Welcome to our stationery haven! Dive into a world of vibrant pens, sleek notebooks, and innovative gadgets as we explore the latest trends in the realm of stationery. From fountain pens to cutting-edge digital planners, our channel offers insightful reviews and recommendations to elevate your writing and organizational experience. Join us in discovering the perfect tools to ignite your creativity and enhance your productivity!</p>
-</div>
+    
+    <div className='para_container'>
+  
+    <div className='para_position'>
+    <p className='para_dash'>Elevate Your Desk.</p>
+    <p className='para_dash'>Ink Your Ideas.</p>
+    <p className='para_dash'>Stationery Stories Unfold.</p>
     </div>
+    <Lottie animationData={animationData} />
+    </div>
+    
+    </div>
+  
     );
 }
 
--- src/navbar/nav.jsx
+++ src/navbar/nav.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import Logo from './logo.png';
 import './nav.css';
 
 function Nav(){
 return (
 <div>
+<div className='nav_position'>
+<div className='logo_div'>
+<a href='https://www.youtube.com/channel/UC5uaixznpO3fl1TuR_g45nw'><img className='logo_nav' src={Logo}></img></a>
+</div>
 <div className='nav_style'>
-    <a href='#' className='nav_anchor'>Home</a>
-    <a href=''  className='nav_anchor'>Youtube</a>
-    <a href=''  className='nav_anchor'>About</a>
-    <a href=''  className='nav_anchor'>contact</a>
+    <a href='#'  className='nav_anchor'>Home</a>
+    <a href='https://www.youtube.com/channel/UC5uaixznpO3fl1TuR_g45nw' className='nav_anchor'>Youtube</a>
+    <a href='#'  className='nav_anchor'>About</a>
+    <a href='#'  className='nav_anchor'>Contact</a>
+    </div>
     </div>
 </div>
 );
