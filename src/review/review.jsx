c4ab4c0d12b15993e0cee9185f622ef3dad7c21e
Himanshu <himanshudey19@gmail.com>
2024-03-11 11:18:11 +0530

    responsive update

--- src/review/review.jsx
+++ src/review/review.jsx
@@ -3,28 +3,60 @@ import './review.css';
 import Card from './reviewcard';
 import { Slide } from "react-awesome-reveal";
 import { Fade } from "react-awesome-reveal";
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { useMediaQuery } from 'react-responsive';
 
 function Review(){
-    return(
-        <section id='initial_sec'>
-        <Slide>
-        <div className='initial_div'>
-        <h1 className='header_review'>"Exquisite Critique: <span className='span_review'>A Showcase of Reviews"</span></h1>
-        </div>
-        </Slide>
-        <div>
-        
-        <div class="container">
-        <Fade>
-        <Card name='Neha' review='A treasure trove of stationery inspiration and tips!'/>
-        <Card name='Dravin' review='Engaging content that sparks creativity and organization.'/>
-        <Card name='Aditi' review='An authentic and informative hub for stationery addicts!'/>
-        </Fade>
-        </div>
-        
-        </div>
-        </section>
-    );
+    const isMobile = useMediaQuery({ maxWidth: 530 });
+
+    if (isMobile) {
+        return (
+            <section id='initial_sec'>
+                <Slide>
+                    <div className='initial_div'>
+                        <h1 className='header_review'>"Exquisite Critique: <span className='span_review'>A Showcase of Reviews"</span></h1>
+                    </div>
+                </Slide>
+                <div className="container">
+                    <Carousel>
+                        <div>
+                            <Card name='Neha' review='A treasure trove of stationery inspiration and tips!'/>
+                        </div>
+                        <div>
+                            <Card name='Dravin' review='Engaging content that sparks creativity and organization.'/>
+                        </div>
+                        <div>
+                            <Card name='Aditi' review='An authentic and informative hub for stationery addicts!'/>
+                        </div>
+                    </Carousel>
+                </div>
+            </section>
+        );
+    } else {
+        return (
+            <section id='initial_sec'>
+                <Slide>
+                    <div className='initial_div'>
+                        <h1 className='header_review'>"Exquisite Critique: <span className='span_review'>A Showcase of Reviews"</span></h1>
+                    </div>
+                </Slide>
+                <div className="container">
+                    <Fade>
+                        <div>
+                            <Card name='Neha' review='A treasure trove of stationery inspiration and tips!'/>
+                        </div>
+                        <div>
+                            <Card name='Dravin' review='Engaging content that sparks creativity and organization.'/>
+                        </div>
+                        <div>
+                            <Card name='Aditi' review='An authentic and informative hub for stationery addicts!'/>
+                        </div>
+                    </Fade>
+                </div>
+            </section>
+        );
+    }
 }
 
 export default Review;
